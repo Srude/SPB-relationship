44f6010cee447e0ffbc6afcbb99bd51894ab250a
오진서 <s22030@gsm.hs.kr>
2023-04-26 18:08:48 +0900

    Entity scan

--- src/main/java/com/spb/relationship/RelationshipApplication.java
+++ src/main/java/com/spb/relationship/RelationshipApplication.java
@@ -2,8 +2,10 @@ package com.spb.relationship;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.spb.relationship.data.entity")
 public class RelationshipApplication {
 
 	public static void main(String[] args) {
